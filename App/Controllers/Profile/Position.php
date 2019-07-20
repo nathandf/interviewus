768159083be56b5efafabad994cf6603e134d9c6
JiuJitsuScout <jiujitsuscout@gmail.com>
2019-07-20 17:15:37 -0500

    Position controller refactore

--- App/Controllers/Profile/Position.php
+++ App/Controllers/Profile/Position.php
@@ -8,78 +8,41 @@ class Position extends Controller
 {
     public function before()
     {
-        $userAuth = $this->load( "user-authenticator" );
-        $countryRepo = $this->load( "country-repository" );
-        $accountRepo = $this->load( "account-repository" );
-        $accountUserRepo = $this->load( "account-user-repository" );
-        $organizationRepo = $this->load( "organization-repository" );
+        if ( !isset( $this->params[ "id" ] ) ) {
+            return [ null, "Error:e404", null, null ];
+        }
+        
         $positionRepo = $this->load( "position-repository" );
 
+        $userAuth = $this->load( "user-authenticator" );
         $this->user = $userAuth->getAuthenticatedUser();
 
         if ( is_null( $this->user ) ) {
-            $this->view->redirect( "sign-in" );
+            return [ null, "DefaultView:redirect", null, "sign-in" ];
         }
 
-        $this->account = $accountRepo->get( [ "*" ], [ "id" => $this->user->current_account_id ], "single" );
-
-        $this->organization = $organizationRepo->get( [ "*" ], [ "id" => $this->user->current_organization_id ], "single" );
-
-        $this->view->assign( "countries", $countryRepo->get( [ "*" ] ) );
-        $this->view->assign( "account", $this->account );
-        $this->view->assign( "organization", $this->organization );
-        $this->view->assign( "user", $this->user );
+        $position = $positionRepo->get(
+            [ "*" ],
+            [
+                "id" => $this->params[ "id" ],
+                "organization_id" => $this->user->current_organization_id
+            ],
+            "single"
+        );
 
         // Ensure the current position is owned by this organization
-        if (
-            isset( $this->params[ "id" ] ) &&
-            !in_array(
-                $this->params[ "id" ],
-                $positionRepo->get( [ "id" ], [ "organization_id" => $this->organization->id ], "raw" )
-            )
-        ) {
-            $this->view->redirect( "profile/" );
+        if ( is_null( $position ) ) {
+            return [ null, "DefaultView:redirect", null, "profile/" ];
         }
     }
 
     public function indexAction()
     {
         if ( !isset( $this->params[ "id" ] ) ) {
-            $this->view->redirect( "profile/" );
+            return [ null, "DefaultView:redirect", null, "profile/" ];
         }
-
 
         $requestValidator = $this->load( "request-validator" );
-        $interviewRepo = $this->load( "interview-repository" );
-        $interviewQuestionRepo = $this->load( "interview-question-repository" );
-        $intervieweeAnswerRepo = $this->load( "interviewee-answer-repository" );
-        $intervieweeRepo = $this->load( "interviewee-repository" );
-        $phoneRepo = $this->load( "phone-repository" );
-        $positionRepo = $this->load( "position-repository" );
-        $deploymentTypeRepo = $this->load( "deployment-type-repository" );
-
-        $position = $positionRepo->get( [ "*" ], [ "id" => $this->params[ "id" ] ], "single" );
-
-        $interviews = array_reverse(
-            $interviewRepo->get(
-                [ "*" ],
-                [
-                    "position_id" => $position->id,
-                    "organization_id" => $this->organization->id
-                ]
-            )
-        );
-
-        foreach ( $interviews as $interview ) {
-            $interview->deploymentType = $deploymentTypeRepo->get( [ "*" ], [ "id" => $interview->deployment_type_id ], "single" );
-            $interview->interviewee = $intervieweeRepo->get( [ "*" ], [ "id" => $interview->interviewee_id ], "single" );
-            $interview->position = $position;
-            $interview->questions = $interviewQuestionRepo->get( [ "*" ], [ "interview_id" => $interview->id ] );
-
-            foreach ( $interview->questions as $question ) {
-                $question->answer = $intervieweeAnswerRepo->get( [ "*" ], [ "interview_question_id" => $question->id ], "single" );
-            }
-        }
 
         if (
             $this->request->is( "post" ) &&
@@ -102,25 +65,9 @@ class Position extends Controller
                 "update_position"
             )
         ) {
-            $positionRepo->update(
-                [
-                    "name" => trim( $this->request->post( "name" ) ),
-                    "description" => trim( $this->request->post( "description" ) )
-                ],
-                [ "id" => $this->params[ "id" ] ]
-            );
-
-            $this->request->addFlashMessage( "success", "Position details updated" );
-            $this->request->setFlashMessages();
-
-            $this->view->redirect( "profile/position/{$this->params[ "id" ]}/" );
+            return [ "Position:update", "DefaultView:redirect", null, "profile/position/{$this->params[ "id" ]}/" ];
         }
 
-        $this->view->assign( "interviews", $interviews );
-        $this->view->assign( "position", $position );
-        $this->view->assign( "flash_messages", $this->request->getFlashMessages() );
-
-        $this->view->setTemplate( "profile/position/index.tpl" );
-        $this->view->render( "App/Views/Index.php" );
+        return [ "Position:index", "Position:index", null, $requestValidator->getErrors() ];
     }
 }
--- App/Views/Position.php
+++ App/Views/Position.php
@@ -2,10 +2,20 @@
 
 namespace Views;
 
-use Core\AbstractView;
-
-class Position extends AbstractView
+class Position extends ProfileView
 {
+	public function index()
+	{
+		$this->validateAccount();
+
+		$this->assign( "interviews", array_reverse( $this->model->interviews ) );
+        $this->assign( "position", $this->model->position );
+        $this->assign( "flash_messages", $this->model->request->getFlashMessages() );
+
+        $this->setTemplate( "profile/position/index.tpl" );
+        $this->render();
+	}
+
 	public function create()
 	{
 		$this->redirect( "profile/position/{$this->model->position->id}/" );
