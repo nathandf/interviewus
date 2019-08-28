a5202296dccdec8ff1aded41dbadf4a7e6caa9a6
JiuJitsuScout <jiujitsuscout@gmail.com>
2019-08-28 14:25:34 -0500

    Load interviewees from interviewee model. Show in view

--- App/Model/Models/Interviewees.php
+++ App/Model/Models/Interviewees.php
@@ -6,6 +6,38 @@ class Interviewees extends ProfileModel
 {
 	public function index()
 	{
-		
+		if ( $this->validateAccount() ) {
+			$intervieweeRepo = $this->load( "interviewee-repository" );
+	        $this->interviewees = $intervieweeRepo->get(
+				[ "*" ],
+				[ "organization_id" => $this->organization->id ]
+			);
+
+			$phoneRepo = $this->load( "phone-repository" );
+			$imageRepo = $this->load( "image-repository" );
+
+			foreach ( $this->interviewees as $interviewee ) {
+
+				// Get phones owned by interviewee
+				$interviewee->phone = null;
+				if ( !is_null( $interviewee->phone_id ) ) {
+					$interviewee->phone = $phoneRepo->get(
+						[ "*" ],
+						[ "id" => $interviewee->phone_id ],
+						"single"
+					);
+				}
+
+				// Get images owned by interviewee
+				$interviewee->image = null;
+				if ( !is_null( $interviewee->image_id ) ) {
+					$interviewee->image = $imageRepo->get(
+						[ "*" ],
+						[ "id" => $interviewee->image_id ],
+						"single"
+					);
+				}
+			}
+		}
 	}
 }
